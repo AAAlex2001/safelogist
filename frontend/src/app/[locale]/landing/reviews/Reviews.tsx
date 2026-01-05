c1f1daa72f89aff053d61a2148cceb896cecad2c
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-01-05 23:37:49 +0300

    свайпер

--- frontend/src/app/[locale]/landing/reviews/Reviews.tsx
+++ frontend/src/app/[locale]/landing/reviews/Reviews.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import React from "react";
 import styles from "./Reviews.module.scss";
 import { Typography } from "@/components/Typography";
+import ReviewCard from "@/components/ReviewCard/ReviewCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
 export default function Reviews() {
   return (
@@ -20,6 +27,30 @@ export default function Reviews() {
           text="Узнайте, как компании ведут себя на деле — по опыту других"
           brown={true}
         />
+      </div>
+
+      <div className={styles.swiperWrap}>
+        <Swiper
+          modules={[Navigation]}
+          navigation={{
+            prevEl: `.${styles.btnPrev}`,
+            nextEl: `.${styles.btnNext}`,
+          }}
+          spaceBetween={24}
+          slidesPerView='auto'
+          className={styles.swiper}
+        >
+          {[...Array(15)].map((_, i) => (
+            <SwiperSlide key={i} className={styles.slide}>
+              <ReviewCard />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <div className={styles.slideBtns}>
+          <button type="button" className={styles.btnPrev} aria-label="Назад" />
+          <button type="button" className={styles.btnNext} aria-label="Вперёд" />
+        </div>
       </div>
     </section>
   );

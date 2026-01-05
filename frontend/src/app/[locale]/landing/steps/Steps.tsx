4d47dede1c4f32a5bd089eadf5d3d8009a8c0726
AAAlex2001 <sasha_tolstov_2001@mail.ru>
2026-01-05 19:49:39 +0300

    правки

--- frontend/src/app/[locale]/landing/steps/Steps.tsx
+++ frontend/src/app/[locale]/landing/steps/Steps.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Typography } from "@/components/Typography";
 import styles from "./Steps.module.scss";
+import { SearchBar} from "@/components/SearchBar";
 
 export function Steps() {
   return (
@@ -20,6 +21,30 @@ export function Steps() {
           text="Минимизируйте риски для вашего бизнеса: мы предоставляем полную информацию, вы принимаете взвешенное решение"
           brown={true}
         />
+      </div>
+      <div className={styles.stepsContainer}>
+        <div className={styles.step}>
+            <Typography
+                as="h3"
+                size={20}
+                desktopSize={20}
+                blue={true}
+                text="Шаг 1 из 3."
+            />
+            <Typography
+                as="h1"
+                size={20}
+                desktopSize={20}
+                blue={true}
+                text="Введите регистрационный/налоговый номер"
+            />
+            <Typography
+                as="h2"
+                size={18}
+                desktopSize={18}
+                text="Укажите интересующую организацию — мы начнём поиск по официальным источникам"
+            />
+        </div>
       </div>
     </section>
   );
